bc480cd7c415fa7b1e1d490721dcf0e193b94fb6
roman.voronov <roman.voronov@axenix.pro>
2024-06-20 21:18:15 +0400

    feature: player can roll through phrases

--- js/code.js
+++ js/code.js
@@ -6,21 +6,53 @@ const phrases_files = [
 const div_main = document.getElementById("main");
 
 let phrases;
+let phrases_index = 0;
 
 
 function get_phrases_json(phrases_file) {
-    return fetch(phrases_folder + phrases_file + '.json')
-        .then((response) => response.json())
-        .then((json) => {
-            phrases = json
-            console.log('From inside function: ' + phrases);
-        });
+    const request = new XMLHttpRequest();
+    request.open("GET", phrases_folder + phrases_file + '.json', false);
+    request.send(null);
+    return JSON.parse(request.responseText);
+}
+
+
+function reveal_full_phrase() {
+    let phrase = phrases[phrases_index];
+    console.log('phrase: ' + phrase);
+    let html = '';
+
+    html += '<div class="phrase_russian_div" onclick="show_phrase_riddle();">' + phrase['russian'] + '</div>';
+    html += '<div class="phrase_russian_hint_div" onclick="show_phrase_riddle();">' + phrase['russian_hint'] + '</div>';
+    html += '<div class="phrase_transcript_div" onclick="show_phrase_riddle();">' + phrase['transcription'] + '</div>';
+    html += '<div class="phrase_armenian_div" onclick="show_phrase_riddle();">' + phrase['armenian'] + '</div>';
+    div_main.innerHTML = html;
+    phrases_index += 1;
+    if (phrases_index == phrases.length) {
+        location.reload();
+    }
+}
+
+
+function show_phrase_riddle() {
+    let phrase = phrases[phrases_index];
+    console.log('phrase: ' + phrase);
+    let html = '';
+
+    html += '<div class="phrase_russian_div" onclick="reveal_full_phrase();">' + phrase['russian'] + '</div>';
+    html += '<div class="phrase_russian_hint_div" onclick="reveal_full_phrase();">' + phrase['russian_hint'] + '</div>';
+    html += '<div class="phrase_hidden_div" onclick="reveal_full_phrase();">(нажми чтобы открыть)</div>';
+    html += '<div class="phrase_hidden_div" onclick="reveal_full_phrase();"></div>';
+    div_main.innerHTML = html;
 }
 
 
 function select_phrases_file(button) {
+    let html = '';
     let name = button.innerText;
-    console.log(name);
+    phrases = get_phrases_json(name);
+    console.log('data: ' + phrases);
+    show_phrase_riddle();
 }
 
 
@@ -35,5 +67,3 @@ function add_phrases_div(name) {
 function entry_point() {
     phrases_files.forEach(add_phrases_div);
 }
-
-//await get_phrases_json(phrases_files[0])
